4380848ae03d82e130d6e453615be41959593c3d
mihsastv <30728392+mihsastv@users.noreply.github.com>
2019-11-26 17:22:53 +0300

    1

---
+++ src/component/Square.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+/*class Square extends React.Component {
+    /!*constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        }
+    }*!/
+    onChangeState = () => {
+        //this.setState({value: 'X'})
+        this.props.onClick()
+    }
+
+    render() {
+        return (
+            <button
+                className="square"
+                onClick={() => this.onChangeState()}>
+                {this.props.value}
+            </button>
+        );
+    }
+}*/
+
+export default Square
